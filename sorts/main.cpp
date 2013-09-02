701f80b9fb5ea606c2ceb822d27387265567ffe6
fanfank <csdbreadstealer@gmail.com>
2013-09-02 15:26:42 +0800

    Added automatic testing functions

--- sorts/main.cpp
+++ sorts/main.cpp
@@ -2,8 +2,65 @@
 int c[1000] = {
     -1, 2, 9, -2, -1, 12, 0, 13, 10, 1, 2, 1, -1, 2, 8, 11, 15, -13, 7, 5
 };
+
+template<typename Comparable>
+void printArr(Comparable a[], int left, int rightp1)
+{
+    if(a == NULL || left < 0 || (left + 1) >= rightp1)
+        return;
+    for(int i = left; i < rightp1; i++)
+        cout<<" "<<a[i];
+    cout<<endl;
+}
+
+void testSortSpeed(int algor = 0)
+{
+    
+}
+
+template<typename T, typename R>
+void testTemplate(R (*func)(T*, int, int))
+{
+    int aa[3] = {0,3,1};
+    func(aa, 0, 3);
+    printArr(aa, 0, 3);
+}
+
+template<typename T, typename R, R (*func)(T*, int, int) >
+void autoTest(int len)
+{
+    time_t bg_time,ed_time;
+    bg_time = time(NULL);
+    int cnt = 10000;
+    int *arr1 = new int[len];
+    int *arr2 = new int[len];
+    while(cnt--)
+    {
+        for(int i = 0; i < len; i++)
+            arr1[i] = arr2[i] = ((int)rand()) % MAX;
+        func(arr1, 0, len);
+        sort(arr2, arr2 + len);
+        for(int i = 0; i < len; i++)
+            if(arr1[i] != arr2[i])
+            {
+                cout<<"Wrong number, cnt is: "<<cnt<<endl;
+                cout<<"arr1:"; printArr(arr1, 0, len);
+                cout<<"arr2:"; printArr(arr2, 0, len);
+                delete []arr1;
+                cout<<endl;
+                delete []arr2;
+                return;
+            }
+
+    }
+    ed_time = time(NULL) ;
+    cout<<"Cost "<<ed_time - bg_time<<" seconds"<<endl;
+    delete []arr1;
+    delete []arr2;
+}
 int main()
 {
+    srand(time(0));
     int a[1000];
     int b[1000];
     int n;
@@ -15,64 +72,108 @@ int main()
     int begin, end, lastn = 20;
     while(cin>>n)
     {
-        if(n > 0)
+        //testTemplate<int, void>(selectionSort);
+        if(n <= 0)
         {
-            lastn = n;
-            for(int i = 0; i < lastn; i++)
-                cin>>a[i];
+            n = lastn;
         }
-
-        for(int i = 0; i < lastn; i++)
-            b[i] = a[i];
-
-        cout<<"Before sorting:"<<endl;
-        for(int i = 0; i < lastn; i++)
-            cout<<b[i]<<" ";
-        cout<<endl;
+        else
+            lastn = n;
 
         cout<<"Which sorting algorithm do you want(0ss, 1is, 2bs, 3hs, 4ms, 5qs, 6ssV2, 7msV2, 8qsV2):";
         cin>>s;
 
-        cout<<"where to begin:"; cin>>begin;
-        cout<<"where is end+1:"; cin>>end;
-        if(end > lastn)
-            end = lastn;
+        char amc;
+        bool am = false;
+        cout<<"Automatic testing 10000 random data?(y/n):";
+        cin>>amc;
+        if(amc == 'y')
+            am = true;
+
+        if(!am)
+        {
+            for(int i = 0; i < lastn; i++)
+                cin>>a[i];
+
+            for(int i = 0; i < lastn; i++)
+                b[i] = a[i];
+
+            cout<<"Before sorting:"<<endl;
+            for(int i = 0; i < lastn; i++)
+                cout<<b[i]<<" ";
+            cout<<endl;
+
+            cout<<"where to begin:"; cin>>begin;
+            cout<<"where is end+1:"; cin>>end;
+            if(end > lastn)
+                end = lastn;
+        }
 
         switch(s)
         {
             case 0:
+                if(am)
+                    autoTest<int, void, selectionSort >(n);
+                else
                 selectionSort(b, begin, end);
                 break;
             case 1:
+                if(am)
+                    autoTest<int, void, insertionSort >(n);
+                else
                 insertionSort(b, begin, end);
                 break;
             case 2:
+                if(am)
+                    autoTest<int, void, bubbleSort >(n);
+                else
                 bubbleSort(b, begin, end);
                 break;
             case 3:
+                if(am)
+                    autoTest<int, void, heapSort >(n);
+                else
                 heapSort(b, begin, end);
                 break;
             case 4:
+                if(am)
+                    autoTest<int, void, mergeSort >(n);
+                else
                 mergeSort(b, begin, end);
                 break;
             case 5:
+                if(am)
+                    autoTest<int, void, quickSort >(n);
+                else
                 quickSort(b, begin, end);
                 break;
             case 6:
+                if(am)
+                    autoTest<int, int, _selectionSort >(n);
+                else
                 _selectionSort(b, begin, end);
                 break;
             case 7:
+                if(am)
+                    autoTest<int, int, _mergeSort >(n);
+                else
                 _mergeSort(b, begin, end);
                 break;
             case 8:
+                if(am)
+                    autoTest<int, int, _quickSort >(n);
+                else
                 _quickSort(b, begin, end);
                 break;
             default:
                 break;
         }
-        for(int i = 0; i < lastn; i++)
-            cout<<b[i]<<" ";
-        cout<<endl;
+        if(!am)
+        {
+            for(int i = 0; i < lastn; i++)
+                cout<<b[i]<<" ";
+            cout<<endl;
+        }
 
         cout<<"Please input # of array elements:";
     }
--- sorts/main.h
+++ sorts/main.h
@@ -1,12 +1,18 @@
+#include <algorithm>
+#include <stdio.h>
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <time.h>
+#include <memory.h>
+#include "selectionsortV2.cpp"
 #include "selectionsort.cpp"
 #include "quicksort.cpp"
 #include "mergesort.cpp"
 #include "heapsort.cpp"
 #include "bubblesort.cpp"
 #include "insertionsort.cpp"
-#include "selectionsortV2.cpp"
 #include "mergesortV2.cpp"
 #include "quicksortV2.cpp"
+#define MAX 0x7fffffff
 using namespace std;
